67730a3656e96deaa6ef106207a13a37c111d624
ankitjain02 <ankit@MacBook-Pro.local>
2015-07-26 16:53:48 +0530

    added new ui for slecting icons

---
+++ Love U Ruchu/VCs/IconImagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  IconImagesViewController.h
+//  
+//
+//  Created by Ankit Jain on 26/07/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IconImagesViewController : UIViewController
+
+@end
---
+++ Love U Ruchu/VCs/IconImagesViewController.m
@@ -0,0 +1,37 @@
+//
+//  IconImagesViewController.m
+//  
+//
+//  Created by Ankit Jain on 26/07/15.
+//
+//
+
+#import "IconImagesViewController.h"
+
+@interface IconImagesViewController ()
+
+@end
+
+@implementation IconImagesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
